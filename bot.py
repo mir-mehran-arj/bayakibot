6aecedb3d4fd5188937e01dc042bf5ee1a84db16
mir-mehran-arj <create.app.android@gmail.com>
2017-06-06 18:41:58 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
+import os
+import bot
+from flask import Flask, request
+from telegram import Update
 
 import sqlite3
 from telegram.ext import Updater
@@ -6,6 +10,33 @@ from telegram.ext import MessageHandler, Filters
 from datetime import datetime
 import logging
 import random
+
+
+application = Flask(__name__, instance_path=os.environ['OPENSHIFT_REPO_DIR'])
+update_queue, bot_instance = bot.setup(webhook_url='https://{}/{}'.format(
+    os.environ['OPENSHIFT_GEAR_DNS'],
+    bot.TOKEN
+))
+
+
+@application.route('/')
+def not_found():
+    """Server won't respond in OpenShift if we don't handle the root path."""
+    return ''
+
+
+@application.route('/' + bot.TOKEN, methods=['GET', 'POST'])
+def webhook():
+    if request.json:
+        update_queue.put(Update.de_json(request.json, bot_instance))
+    return ''
+
+
+if __name__ == '__main__' or __name__ == '__bot__':
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
+    port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+application.run(host=ip, port=port)
+
 
 def r():
     a = random.randint(50,100)
